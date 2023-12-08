e776af08502884dab9a53d1766c1afe472281821
Sofia <sofiy.gavrilenko@gmail.com>
2023-12-08 16:02:22 +0700

    Сделана главная страница планов

--- frontend/src/app/components/plan/plan.component.ts
+++ frontend/src/app/components/plan/plan.component.ts
@@ -39,4 +39,25 @@ export class PlanComponent implements OnInit {
       }
     });
   }
+
+  function (): void {
+    const headings = document.querySelectorAll('h5');
+
+    Array.prototype.forEach.call(headings, h => {
+      let btn = h.querySelector('button');
+      let target = h.nextElementSibling;
+
+      btn.onclick = () => {
+        let expanded = btn.getAttribute('aria-expanded') === 'true';
+        /*document.querySelector('.col-md-9').style.backgroundColor = '#f0f0f0';*/
+
+        btn.setAttribute('aria-expanded', !expanded);
+        target.hidden = expanded;
+      }
+    });
+  }
+
+
+
+
 }
