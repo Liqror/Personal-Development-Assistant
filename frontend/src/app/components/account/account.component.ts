f35f113e28cb231773694f8a115c1a6c58e7f11b
Nazarova_Vladislava <nvv.03@yandex.ru>
2025-05-10 20:41:32 +0700

    Доработано окно Профиль

--- frontend/src/app/components/account/account.component.ts
+++ frontend/src/app/components/account/account.component.ts
@@ -2,7 +2,6 @@ import { OnInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { formatDate } from '@angular/common';
 
 
-
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -14,6 +13,18 @@ export class AccountComponent implements OnInit {
   myScriptElement: HTMLScriptElement;
 
   todayDate: string; // Форматированная текущая дата
+
+
+  notesHome: boolean = true;
+  timetableHome: boolean = true;
+  deadlineAuto: boolean = true;
+  deadlineDates: string;
+  dividingWeeks: boolean = true;
+  dividingWeeksDate: string;
+  autoTimeZone: boolean = true;
+  userTimeZone: string;
+  estimate:number = 100;
+
 
   constructor() {
     // Получение текущей даты в формате YYYY-MM-DD
